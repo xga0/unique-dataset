63f6147198b57c80054b77378a456f3abe5162fb
Xiangyu Gao <seangaoxy@gmail.com>
2020-02-20 20:46:05 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 20 20:14:21 2020
+
+@author: sgaox
+"""
+
+from flask import Flask, request, send_file
+
+def uniqueDataset(ID):
+    import pandas as pd
+    
+    house = pd.read_csv(r'C:\Users\sgaox\Desktop\ud\house.csv')
+    house_sub = house.sample(n = 800, random_state = ID)
+    return house_sub
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def udataset():
+    if request.method == "POST":
+        ID = int(request.form['ID'])
+        df = uniqueDataset(ID)
+        df.to_csv('house.csv', index = False)
+        return send_file('house.csv',
+                     mimetype = 'text/csv',
+                     attachment_filename = 'house.csv',
+                     as_attachment = True)
+    
+    return '''
+        <html>
+            <body>
+                <p>Enter Your NJIT ID:</p>
+                <form method="post" action=".">
+                    <p><input name="ID" /></p>
+                    <p><input type="submit" value="Download Your Dataset" /></p>
+                </form>
+            </body>
+        </html>
+    '''
+    
+if __name__ == "__main__":
+    app.run(debug = True)
